3e5869a6ad918adca4cf0f4908ea8e8e745c77ff
Géczi Kornél <hungarokornel@gmail.com>
2023-05-23 18:25:43 +0200

    Add project files.

---
+++ 2023-05-19/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_05_19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
